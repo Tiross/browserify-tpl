75d42cb72f259a587115ad824708ec8c14056547
Marcos Peron <mperon@outlook.com>
2018-06-23 23:07:00 -0300

    First version of modified browserify transform..

--- README.md
+++ README.md
@@ -33,12 +33,21 @@ var html = myTemplate({title: "An instantiated template!", name: "John"});
 
 To work, run *browserify* with the transform option:
 
-`browserify -t browserify-handlebars entry-point.js`
+`browserify -t browserify-tpl entry-point.js`
 
 Or using *gulp* like this:
 
 ```javascript
-
+gulp.task("scripts", function () {
+    return browserify("**/*.js")
+          .transform('browserify-tpl', {
+              'patterns': ['.tpl', '.mustache', '.hg'],
+              'engine': 'handlebars'
+          });
+          .bundle()
+          .pipe(source('bundle.js'))
+          .pipe(dest('build/'));
+});
 ```
 
 That's all!
@@ -47,7 +56,38 @@ That's all!
 
 This transform module packages the handlebars templates with the handlebars runtime, which is smaller than the complete handlebars library. This is good, because it means smaller bundle files for you.
 
+## Next (SOOON!)
+
+We will support multiples templates inside a require. Something like:
+
+```html
+<!DOCTYPE html>
+  <html lang="en">
+  <head></head>
+  <body>
+  <script id="helloWorld" type="text/html">
+    Hello World {{name}}
+  </script>
+  <script id="goodMorning" type="text/html">
+    Good Morning {{name}}
+  </script>
+  <script id="goodAfternoon" type="text/html">
+    Good Afternoon {{name}}
+  </script>
+  </body>
+  </html>
+```
+And then use in code like this:
+
+```javascript
+var Tpl = require('./template.html');
+
+var htmlHelloWorld = Tpl.helloWorld({name: "John"});
+var htmlGoodMorning = Tpl.goodMorning({name: "John"});
+//and so on..
+```
+
 ## Credits
 
-This transform is a fork on David Manning [browserify-handlebars](https://github.com/dlmanning/browserify-handlebars) ! 
+This transform is a fork on David Manning [browserify-handlebars](https://github.com/dlmanning/browserify-handlebars) !
 Thanks!
--- index.js
+++ index.js
@@ -1,26 +1,100 @@
-var handlebars = require('handlebars');
 var through = require('through');
 
-var filenamePattern = /\.(html|handlebars|hbs)$/;
+var defaultPatterns = ['.tpl', '.mustache', '.hb', '.html'];
 
-var wrap = function (template) {
-  return 'var templater = require("handlebars/runtime")["default"].template;' +
-         'module.exports = templater(' + template + ');'
+var defaultEngine = 'handlebars';
+
+var defaultEngines = {
+    handlebars: function (input, opts) {
+        var handlebars = require('handlebars');
+
+        var compilerOptions = opts.compilerOptions ?
+            opts.compilerOptions : {};
+
+        var compiled = handlebars.precompile(input, compilerOptions);
+        var out = [
+            'var hb = require("handlebars/runtime");',
+            'module.exports = hb.template(' + compiled + ');'
+        ];
+        //'var templater = require("handlebars/runtime")["default"].template;' +
+        return out.join("\n");
+    },
+    hogan: function(input, opts) {
+        var hogan = require('jogan.js');
+        var defOpts = {
+            asString: 1,
+            enableHelpers: 1,
+            fixMethodCalls: 1
+        };
+        var hogOpts = opts.compilerOptions ? opts.compilerOptions : defOpts;
+        var compiled = hogan.compile(input, hogOpts)
+        var out = [
+            'var hogan = require("hogan.js");',
+            'var n = new hogan.Template(' + compiled + ');',
+            'module.exports = function(data, partials) {',
+            '  return n.render(data, partials);',
+            '};'
+        ];
+        return out.join("\n");
+    },
+    mustache: function(input, opts) {
+        var compiled = JSON.stringify(input);
+        var out = [
+           'var mustache = require("mustache");',
+           'module.exports = function(data) {',
+           '    var tpl = '+compiled+';',
+           '    mustache.parse(tpl);',
+           '    return mustache.render(tpl, data);',
+           '};'
+        ];
+        return out.join("\n");
+    }
+};
+
+function patternMatcher(search, patterns) {
+    return patterns.find(function (el) {
+        return search.endsWith(el);
+    }) !== undefined;
 }
 
-module.exports = function (file) {
-  if (!filenamePattern.test(file)) return through();
+module.exports = function (file, opts) {
+    //check if user passes patterns to filter
+    var patterns = opts.patterns ? opts.patterns : defaultPatterns;
+    if (typeof patterns === "string") {
+        patterns = [patterns];
+    }
+    //if is not an array
+    if (!Array.isArray(patterns)) {
+        throw new Error('Option patterns must be a string or an array!');
+    }
+    // if path doesnt matches, just pass through
+    if (!patternMatcher(file, patterns)) return through();
 
-  var input = '';
-  var write = function (buffer) {
-    input += buffer;
-  }
+    //read data and passes to compiler
+    var input = '';
+    var write = function (buffer) {
+        input += buffer;
+    }
 
-  var end = function () {
-    this.queue(wrap(handlebars.precompile(input)));
-    this.queue(null);
-  }
+    var end = function () {
+        this.queue(compileTemplate(input));
+        this.queue(null);
+    }
 
-  return through(write, end);
-
+    var compileTemplate = function (data) {
+        var engine = opts.engine ? opts.engine : defaultEngine;
+        //handle engines
+        if (typeof engine === "string") {
+            var engine = Object.keys(defaultEngines).find(function(key) {
+                return strEngine.toLowerCase() === key;
+            });
+            if (engine) {
+                return defaultEngines[engine](data, opts);
+            }
+        } else if (typeof engine === 'function') {
+            //user is providing a custom function to compile and parse data
+            return engine(data, opts.compilerOptions ? opts.compilerOptions : {});
+        }
+    }
+    return through(write, end);
 }
